46d3a4fabc1bbb3b94729e630549761dcfd81532
Stephen Chalker <stephen@bypassmobile.com>
2020-05-19 19:08:12 -0500

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Stephen Chalker on 5/19/20.
+//  Copyright © 2020 Stephen Chalker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
